20f6aacaa576dec6fe6e6d242164b7d78cd8f5c4
Nicolas Torgersen-Montijo <ntorger3@gmail.com>
2022-03-16 15:40:47 -0600

    Completed Project upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# magic_8_ball
+
+This is a magic 8 ball app. You tap on the ball with a question in mind, and it will give you a random answer, as a real magic 8 ball would.
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
+      ),
+    );
+
+// ignore: use_key_in_widget_constructors
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+// ignore: use_key_in_widget_constructors
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        style: ButtonStyle(
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
+        ),
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
